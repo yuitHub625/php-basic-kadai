b0327a86762bdef2216ee23acb8b94db227f2cae
yuitHub625 <luckylukia0626@gmail.com>
2025-05-08 04:23:49 +0900

    kadai_005を編集

---
+++ kadai_005/score.php
@@ -0,0 +1,21 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+// 合計点を計算
+$total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+echo "合計点: " . $total . "<br>";
+
+// 平均点を計算
+$average = $total / 10;
+echo "平均点: " . $average . "<br>";
+
+?>
