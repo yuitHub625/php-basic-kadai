d5b10f9d2cde0ed925edcb2268c61f290c68a7b4
yuitHub625 <luckylukia0626@gmail.com>
2025-05-09 05:27:00 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_007</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+       // 連想配列の値を出力する
+       print_r($vegetable_data);
+       ?>
+   </p>
+</body>
+
+</html>
